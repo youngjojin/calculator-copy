e2a137db3bd6ec4011428615f466a535c848b8ba
uunseo <uunseoy@gmail.com>
2026-03-09 20:36:19 +0900

    Add division function

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add, subtract, multiply
+from calculator import add, subtract, multiply, divide
 
 
 def test_add():
@@ -11,3 +11,7 @@ def test_subtract():
 
 def test_multiply():
     assert multiply(3, 4) == 12
+
+
+def test_divide():
+	assert divide(10, 2) == 5
